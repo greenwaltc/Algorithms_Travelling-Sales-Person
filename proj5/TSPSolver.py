7698c35f83598654d22c4a0d7185a399b30bb783
Cameron Greenwalt <camerongreenwalt@gmail.com>
2021-04-07 12:26:05 -0600

    cameron pc

--- proj5/TSPSolver.py
+++ proj5/TSPSolver.py
@@ -2,6 +2,9 @@
 
 from which_pyqt import PYQT_VER
 from queue import PriorityQueue
+import numpy as np
+import copy
+import sys
 import random
 
 if PYQT_VER == 'PYQT5':
@@ -162,10 +165,11 @@ class TSPSolver:
 		results = {}
 		self.cities = self._scenario.getCities()
 		self.ncities = len(self.cities)
-		lowerBound = 0
+		self.lowerBound = 0
+		self.state_num = 0
 
 		'''Initialize BSSF to a random solution'''
-		bssf = self.defaultRandomTour(time_allowance)['soln']
+		self.bssf = self.defaultRandomTour(time_allowance)['soln']
 
 		'''Initialize state priority queue'''
 		stateQueue = PriorityQueue()
@@ -173,23 +177,72 @@ class TSPSolver:
 		'''	Create the root of the state tree
 			Reduce the cost matrix
 			Set lower bound to cost of first reduction'''
-		root = state(self.ncities)
-		self.initializeState(root)
-		lowerBound = root.cost
+		root = self.state(self.ncities)
+		self.initializeState(None, root)
+		root.city_num = 0  # Always start at first city
+		root.path.append(root.city_num)
+		self.lowerBound = root.cost
+
+		stateQueue.put((1/root.cost, root))  # By putting the inverse of the cost of the state, the max heap queue is treated like a min-heap
+
+		'''Begin the algorithm'''
+		while not stateQueue.empty():
+			state = stateQueue.get()[1]
+			if state.cost < self.bssf.cost:
+
+				'''Make each child state'''
+				for j in range(self.ncities):
+					if state.cost_matrix[state.city_num][j] != math.inf:
+						# There is a path from this city to the next
+
+						'''Set up initial values for child'''
+						child = self.state(self.ncities)
+						state.children.append(child)
+						child.parent = state
+						child.state_num = self.state_num
+						self.state_num += 1
+						child.city_num = j
+						child.depth = child.parent.depth + 1
+						child.cost_matrix = copy.deepcopy(child.parent.cost_matrix) # Don't want the parent's matrix values to change
+						child.path = child.parent.path
+						child.path.append(j)
+
+						'''Inf out appropriate row and column'''
+						row = child.parent.city_num
+						col = child.city_num
+						for k in range(self.ncities):
+							child.cost_matrix[row][k] = math.inf
+						for k in range(self.ncities):
+							child.cost_matrix[k][col] = math.inf
+
+						'''Prevent premature cycles'''
+						path_len = len(child.path)
+						index = path_len - 1
+						while index >= 0:
+							child.cost_matrix[child.path[path_len - 1]][index] = math.inf
+							index -= 1
+
+						'''Calculate State Cost'''
+						cost_reduction = self.reduceMatrix(child)
+						cost_step = child.parent.cost_matrix[child.parent.city_num][child.city_num]
+						cost_prev_state = child.parent.cost
+						child.cost = cost_prev_state + cost_step + cost_reduction
 
 		pass
 
-	def initializeState(self, state):
+	def initializeState(self, parent, child):
 		# This is the root of the state tree, or state one
-		if state.parent == None:
-			state.state_num = 1
-			state.depth = 1
+		if parent == None:
+			child.city_num = 0  # first state assumes always starting at first city
+			child.state_num = self.state_num
+			self.state_num += 1
+			child.depth = 1
 			'''Initialize first state cost matrix'''
 			for i in range(self.ncities):
 				for k in range(self.ncities):
-					state.cost_matrix[i][k] = self.cities[i].costTo(self.cities[k])
+					child.cost_matrix[i][k] = self.cities[i].costTo(self.cities[k])
 			'''Reduce the cost matrix'''
-			state.cost = self.reduceMatrix(state)
+			child.cost = self.reduceMatrix(child)
 
 		pass
 
@@ -238,15 +291,114 @@ class TSPSolver:
 	def fancy(self, time_allowance=60.0):
 		pass
 
-class state:
-	'''
-		n:param the number of nodes in the graph
-	'''
-	def __init__(self, n):
-		self.state_num = -1
-		self.parent = None
-		self.children = []
-		self.cost = -1
-		self.city = None
-		self.depth = 0
-		self.cost_matrix = [[-1 for i in range(n)] for k in range(n)]
+	class state:
+		'''
+			n:param the number of nodes in the graph
+		'''
+		def __init__(self, n):
+			self.state_num = -1
+			self.parent = None
+			self.children = []
+			self.cost = -1
+			self.city_num = None
+			self.depth = 0
+			self.cost_matrix = [[-1 for i in range(n)] for k in range(n)]
+			self.path = []  # Stores the path so far in terms of city indexes
+
+
+# class MinHeap:
+#
+# 	def __init__(self, maxsize):
+# 		self.maxsize = maxsize
+# 		self.size = 0
+# 		self.Heap = [0] * (self.maxsize + 1)
+# 		self.Heap[0] = -1 * sys.maxsize
+# 		self.FRONT = 1
+#
+# 	# Function to return the position of
+# 	# parent for the node currently
+# 	# at pos
+# 	def parent(self, pos):
+# 		return pos // 2
+#
+# 	# Function to return the position of
+# 	# the left child for the node currently
+# 	# at pos
+# 	def leftChild(self, pos):
+# 		return 2 * pos
+#
+# 	# Function to return the position of
+# 	# the right child for the node currently
+# 	# at pos
+# 	def rightChild(self, pos):
+# 		return (2 * pos) + 1
+#
+# 	# Function that returns true if the passed
+# 	# node is a leaf node
+# 	def isLeaf(self, pos):
+# 		if pos >= (self.size // 2) and pos <= self.size:
+# 			return True
+# 		return False
+#
+# 	# Function to swap two nodes of the heap
+# 	def swap(self, fpos, spos):
+# 		self.Heap[fpos], self.Heap[spos] = self.Heap[spos], self.Heap[fpos]
+#
+# 	# Function to heapify the node at pos
+# 	def minHeapify(self, pos):
+#
+# 		# If the node is a non-leaf node and greater
+# 		# than any of its child
+# 		if not self.isLeaf(pos):
+# 			if (self.Heap[pos] > self.Heap[self.leftChild(pos)] or
+# 					self.Heap[pos] > self.Heap[self.rightChild(pos)]):
+#
+# 				# Swap with the left child and heapify
+# 				# the left child
+# 				if self.Heap[self.leftChild(pos)] < self.Heap[
+# 					self.rightChild(pos)]:
+# 					self.swap(pos, self.leftChild(pos))
+# 					self.minHeapify(self.leftChild(pos))
+#
+# 				# Swap with the right child and heapify
+# 				# the right child
+# 				else:
+# 					self.swap(pos, self.rightChild(pos))
+# 					self.minHeapify(self.rightChild(pos))
+#
+# 	# Function to insert a node into the heap
+# 	def insert(self, element):
+# 		if self.size >= self.maxsize:
+# 			return
+# 		self.size += 1
+# 		self.Heap[self.size] = element
+#
+# 		current = self.size
+#
+# 		while self.Heap[current] < self.Heap[self.parent(current)]:
+# 			self.swap(current, self.parent(current))
+# 			current = self.parent(current)
+#
+# 	# Function to print the contents of the heap
+# 	def Print(self):
+# 		for i in range(1, (self.size // 2) + 1):
+# 			print(" PARENT : " + str(self.Heap[i]) + " LEFT CHILD : " +
+# 				  str(self.Heap[2 * i]) + " RIGHT CHILD : " +
+# 				  str(self.Heap[2 * i + 1]))
+#
+# 	# Function to build the min heap using
+# 	# the minHeapify function
+# 	def minHeap(self):
+#
+# 		for pos in range(self.size // 2, 0, -1):
+# 			self.minHeapify(pos)
+#
+# 	# Function to remove and return the minimum
+# 	# element from the heap
+# 	def remove(self):
+#
+# 		popped = self.Heap[self.FRONT]
+# 		self.Heap[self.FRONT] = self.Heap[self.size]
+# 		self.size -= 1
+# 		self.minHeapify(self.FRONT)
+# 		return popped
